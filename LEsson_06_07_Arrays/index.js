0509fb5e32e24b169d16514f85178326b01f9932
Bogdan <youbody7@gmail.com>
2022-07-06 15:09:33 +0300

    s

--- LEsson_06_07_Arrays/index.js
+++ LEsson_06_07_Arrays/index.js
@@ -30,4 +30,31 @@ const mapResult = numbersList.map((el, index) => {
 // input callback
 // output undefined
 
-numbersList.forEach((el) => {});
+// task 3
+const res = numbersList.forEach((el) => {
+  if (el > 0) {
+    console.log(el);
+  }
+});
+console.log(res);
+const forEachArray = numbersList.forEach((el) => el * el);
+
+// task 4 reduce
+
+const transactions = [5, 0, 8, 10, -4, 50, 220, 1203, 556, 41];
+
+const reduceArray = transactions.reduce((sum, el) => {
+  if (el > 100) {
+    return sum + el;
+  }
+  return sum;
+}, 0);
+
+console.log(reduceArray);
+
+// option 2
+
+const reduceArray2 = transactions.reduce(
+  (sum, el) => (el > 100 ? sum + el : el),
+  0
+);
